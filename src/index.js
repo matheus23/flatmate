6bc68a03992d3cf67a2d3041614ffaf82d31dbf8
Philipp Krüger <philipp.krueger1@gmail.com>
2020-07-14 10:38:22 +0200

    :package: Use create-elm-app and eject

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import './main.css';
 import { Elm } from './Main.elm';
-import 'babel-polyfill'
-import './main.ts';
+import * as serviceWorker from './serviceWorker';
+import IPFS from 'ipfs';
+import OrbitDB from 'orbit-db';
 
 Elm.Main.init({
-    node: document.querySelector('main')
+    node: document.getElementById('root')
 });
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
