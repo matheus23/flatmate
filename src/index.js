843b758876c93daa453c2b3ca9721f431401047f
Philipp Krüger <philipp.krueger1@gmail.com>
2020-07-23 17:03:07 +0200

    :package: Successfully install and run orbit-db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import '../css/style.css'
 import { Elm } from './Main.elm';
+import IPFS from 'ipfs'
+import OrbitDB from 'orbit-db'
 
 Elm.Main.init({
     node: document.querySelector('body')
 });
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // IPFS node setup
+    const node = await IPFS.create({ repo: String(Math.random() + Date.now()) })
+
+    function log(txt) {
+        console.info(txt)
+    }
+
+    const version = await node.version()
+
+    log(`The IPFS node version is ${version.version}`)
+
+
+    const orbitdb = await OrbitDB.createInstance(node)
+    const db = await orbitdb.log('first-database')
+    log(`db: ${db.address} running`)
+    db.add("test", "asd")
+    db.events.on('replicated', () => {
+        const res = db.iterator({ limit: -1 }).collect().map(e => e.payload.value)
+        log(`replicated! ${res}`)
+    })
+    setInterval(async () => {
+        await db.add('test', "" + new Date().getTime())
+    }, 1000)
+})
